bb391df9b8e151b3483e88903efa17c63a4c94c4
Hartono Pratama <hptaurus88@gmail.com>
2014-06-13 03:17:14 +0700

    Buat Menu Makanan dan Tambah Makanan

---
+++ Aplikasi Transaksi Penjualan/Menu Makanan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplikasi_Transaksi_Penjualan
+{
+    public partial class Menu_Makanan : Form
+    {
+        public Menu_Makanan()
+        {
+            InitializeComponent();
+        }
+    }
+}
